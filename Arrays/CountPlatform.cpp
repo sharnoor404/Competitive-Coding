d8d98df664a0986abc1bbea253c500328b6c5c90
sharnoor404 <sharnoor404@gmail.com>
2020-06-10 23:54:58 +0530

    code

---
+++ Arrays/CountPlatform.cpp
@@ -0,0 +1,30 @@
+/*Given two arrays (both of size n), one containing arrival time of trains 
+and other containing the corresponding trains departure time (in the form of an integer) respectively. 
+Return the minimum number of platform required, such that no train waits.
+Arrival and departure time of a train can't be same.
+*/
+#include<algorithm>
+int platformsNeeded(int arrival[], int departure[], int n) {
+    int count=0;
+    int max_count=0;
+    sort(arrival,arrival+n);
+    sort(departure,departure+n);
+    
+
+    int i=0;
+    int j=0;
+    
+    while(i<n && j<n){
+        if(arrival[i]<departure[j]){
+            count++;
+            i++;
+            if(count>max_count){
+                max_count=count;
+            }
+        }else{
+            count=0;
+               j++;  
+        }
+    }
+    return max_count;
+}
