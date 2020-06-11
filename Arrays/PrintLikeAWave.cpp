eaf880e64e70f8f430dcc6a8bb84a90b7c9bf6a4
sharnoor404 <sharnoor404@gmail.com>
2020-06-11 23:32:40 +0530

    code

---
+++ Arrays/PrintLikeAWave.cpp
@@ -0,0 +1,23 @@
+/*
+For a given two-dimensional integer array/list of size (N x M), 
+print the array/list in a sine wave order, 
+i.e, print the first column top to bottom, 
+next column bottom to top and so on.
+*/
+
+void wavePrint(int **input, int nRows, int mCols)
+{
+   
+    for(int j=0;j<mCols;j++){
+        if(j%2==0){
+         for(int i=0;i<nRows;i++){
+            cout<<input[i][j]<<" ";
+        }   
+        }else{
+          for(int i=nRows-1;i>=0;i--){
+            cout<<input[i][j]<<" ";
+        }    
+        }
+        
+    }
+}
