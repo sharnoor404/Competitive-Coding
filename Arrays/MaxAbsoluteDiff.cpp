86c734de26342dfafe98669e60cff3b9f684f10e
sharnoor404 <sharnoor404@gmail.com>
2020-06-14 23:28:06 +0530

    code

--- Arrays/MaxAbsoluteDiff.cpp
+++ Arrays/MaxAbsoluteDiff.cpp
@@ -1,3 +1,7 @@
+/*
+Given an array of N integers. Return maximum value of f(i, j) for all 1 = i, j = N.
+f(i, j) is defined as |A[i] - A[j]| + |i - j|, where |x| denotes absolute value of x.
+*/
 int Solution::maxArr(vector<int> &A) {
     
     int max1=INT_MIN;
