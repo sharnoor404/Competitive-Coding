609cc24c1c5c985eea0f2a5c5330f779db50d36a
sharnoor404 <sharnoor404@gmail.com>
2020-06-16 20:42:19 +0530

    code

---
+++ Math/AllFactors.cpp
@@ -0,0 +1,23 @@
+/*
+Given a number N, find all factors of N.
+*/
+vector<int> Solution::allFactors(int A) {
+    
+    vector<int> v;
+    
+    for(int i=1;i<=sqrt(A);i++){
+        if(A%i==0){
+            v.push_back(i);
+        }
+    }
+    
+    for(int i=v.size()-1;i>=0;i--){
+        if(v[i]!=sqrt(A)){
+             v.push_back(A/v[i]);
+        }
+       
+    }
+    return v;
+}
+
+
