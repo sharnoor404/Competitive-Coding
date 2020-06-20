adf18daa75261d9edcbb9adf5060b453e1d3297b
sharnoor404 <sharnoor404@gmail.com>
2020-06-20 13:46:06 +0530

    code

--- Arrays/SmallestPositiveMissingNumber.cpp
+++ Arrays/SmallestPositiveMissingNumber.cpp
@@ -23,3 +23,40 @@ int findMissing(int arr[], int n) {
         }
     }
 }
+
+//approach 2: space efficient
+int findMissing(int arr[], int n) { 
+    
+      for(int i=0;i<n;i++){
+          //this uses index referencing,
+          //i.e every index would have index+1 as value 
+          if(arr[i]>0 && arr[i]<=n){
+              int pos=arr[i]-1;
+              //swap
+              if(arr[pos]!=arr[i]){
+                int temp=arr[pos];
+                arr[pos]=arr[i];
+                arr[i]=temp;
+                i--;
+              }
+          }
+      }
+      
+      for(int i=0;i<n;i++){
+          if(arr[i]!=i+1){
+              return i+1;
+          }
+      }
+      return n+1;
+}
+//Dry run:
+//Given: 0 -10 1 3 -20
+//1>0 && 1<=5: pos=1-1 swap(arr[pos] and arr[i])
+// after swap: 1 -10 0 3 -20
+//3>0 && 3<=5 : pos=3-1 
+//after swap: 1 -10 3 0 -20
+//inde:       0  1  2 3  4
+
+//check which index doesnt have a value as index+1
+// ans=1+1: 2 
+
