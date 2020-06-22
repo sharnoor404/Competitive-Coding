d6d14906db42758ad765dbaa45bd8ab427a408b1
sharnoor404 <sharnoor404@gmail.com>
2020-06-23 00:31:05 +0530

    code

--- Arrays/VerticalOrder.cpp
+++ Arrays/VerticalOrder.cpp
@@ -1,31 +1,29 @@
-/*
-Given a binary tree, print that binary tree in vertical order.
-*/
 #include<unordered_map>
-#include<vector>
- void print(BinaryTreeNode<int> *root, int order, unordered_map<int,vector<int>> &ourmap)
-    {
-     if(root==NULL){
-         return;
-     }
-     ourmap[order].push_back(root->data);
-        print(root->left,order-1,ourmap);
-        print(root->right,order+1,ourmap);
+
+void helper(BinaryTreeNode<int>* root,int key,unordered_map<int,vector<int>>& ourmap){
+    if(root==NULL){
+        return;
     }
-	void printBinaryTreeVerticalOrder(BinaryTreeNode<int>* root) {	 
-     	unordered_map<int,vector<int>> ourmap;
-        int order=0;
-        print(root,order,ourmap);
+    
+    ourmap[key].push_back(root->data);
+    //traverse left
+    helper(root->left,key-1,ourmap);
+    //traverse right
+    helper(root->right,key+1,ourmap);
+    
+}
+
+void printBinaryTreeVerticalOrder(BinaryTreeNode<int>* root) {	 
+		unordered_map<int,vector<int>> ourmap;
+		int key=0;
         
-     	unordered_map<int,vector<int>> :: iterator it=ourmap.begin();
-     while(it!=ourmap.end()){
-       //  int k=it->second.size();
-         for(int i=0;i<it->second.size();i++){
-             cout<<it->second[i]<<" ";
-         }
-         cout<<endl;
-         it++;
-     }	
- 	}
-
-
+        helper(root,key,ourmap);
+    	unordered_map<int,vector<int>>:: iterator it=ourmap.begin();
+    	while(it!=ourmap.end()){
+            for(int i=0;i<it->second.size();i++){
+                cout<<it->second[i]<<" ";
+            }
+            cout<<endl;
+            it++;
+        }
+}
