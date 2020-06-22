16112c0d339b6f0d8ade3d5202bd44c834b4d69e
sharnoor404 <sharnoor404@gmail.com>
2020-06-22 23:59:45 +0530

    code

--- Arrays/LongestSubset.cpp
+++ Arrays/LongestSubset.cpp
@@ -1,6 +1,9 @@
 /*
 Given an array, find and return the length of longest subarray containing equal number of 0s and 1s.
 */
+
+//approach 1:
+
 #include<unordered_map>
 int max(int arr[], int n) {
     unordered_map<int,int> ourmap;
@@ -37,4 +40,48 @@ int max(int arr[], int n) {
   
     return max_length;
 }
+
+
+//approach 2:
+
+#include<unordered_map>
+#include<vector>
+int max(int arr[], int n) {
+	unordered_map<int,vector<int>> ourmap;
+    vector<int> x;
+    int sum_till_now=0;
+    x.push_back(sum_till_now);
+    
+    for(int i=0;i<n;i++){
+        if(arr[i]==0){
+            sum_till_now--;
+            x.push_back(sum_till_now);
+        	
+        }else if(arr[i]==1){
+            sum_till_now++;
+            x.push_back(sum_till_now);
+        	
+        }
+        
+    }
+    
+
+    for(int i=0;i<x.size();i++){
+        ourmap[x[i]].push_back(i);
+    }
+    
+    unordered_map<int,vector<int>>:: iterator it=ourmap.begin();
+    int max_length=0;
+    while(it!=ourmap.end()){
+        if(it->second.size()>=2){
+            if((it->second[it->second.size()-1])-(it->second[0])>max_length){
+                max_length=(it->second[it->second.size()-1])-(it->second[0]);
+            }
+            
+        }
+        it++;
+    }
+    return max_length;
+}
+
 
