06824ad9414c7983ca0c755104a0da56ff011db3
sharnoor404 <sharnoor404@gmail.com>
2020-06-26 13:53:45 +0530

    code

---
+++ Trees/FindNode.cpp
@@ -0,0 +1,28 @@
+/*
+Given a Binary Tree and an integer x, 
+check if node with data x is present in the input binary tree or not. 
+Return true or false.
+*/
+bool isNodePresent(BinaryTreeNode<int>* root, int x) {
+
+    if(root==NULL){
+        return false;
+    }
+    
+    if(root->data==x){
+        return true;
+    }
+    
+    if(isNodePresent(root->left,x)){
+        return true;
+    }else{
+        if(isNodePresent(root->right,x)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+}
+
+
