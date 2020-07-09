e947d5958c198a62c365ce454d21557455e3bcbd
sharnoor404 <sharnoor404@gmail.com>
2020-07-09 21:31:00 +0530

    code

---
+++ Trees/Bulbs.cpp
@@ -0,0 +1,19 @@
+/*
+Bulbs problem 
+*/
+int Solution::bulbs(vector<int> &A) {
+    int look_for=0;
+    int count=0;
+    for(int i=0;i<A.size();i++){
+        if(A[i]==look_for){
+            count++;
+            if(look_for==0){
+            look_for=1;
+            }else{
+                look_for=0;
+            }
+        }
+    }
+    return count;
+}
+
