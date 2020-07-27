b5bd76c82d42a52b826d0f869c4e171592400021
sharnoor404 <sharnoor404@gmail.com>
2020-07-28 00:41:37 +0530

    GFG

--- Arrays/SortedMatrix.cpp
+++ Arrays/SortedMatrix.cpp
@@ -1,10 +1,13 @@
 /*
 Given an N*N matrix (sorted row wise and column wise), print elements in sorted fashion.
 */
+
 #include <iostream>
 #include<vector>
 #include<queue>
 using namespace std;
+
+typedef pair<int,pair<int,int>> P;
 
 int main() {
 
@@ -15,27 +18,42 @@ int main() {
         
         int N;
         cin>>N;
-        priority_queue<int,vector<int>,greater<int>> Q;
+        int **arr=new int*[N];
+        for(int i=0;i<N;i++){
+            arr[i]=new int[N];
+            for(int j=0;j<N;j++){
+                cin>>arr[i][j];
+            }
+        }
+        
+        priority_queue<P,vector<P>,greater<P>> Q;
         
         for(int j=0;j<N;j++){
-            int x;
-            cin>>x;
-            Q.push(x);
-        }
-        for(int j=N;j<N*N;j++){
-            cout<<Q.top()<<" ";
-            Q.pop();
-            int x;
-            cin>>x;
-            Q.push(x);
+            P temp;
+            temp.first=arr[j][0];
+            temp.second.first=j;
+            temp.second.second=0;
+            Q.push(temp);
         }
         
-        while(!Q.empty()){
-            cout<<Q.top()<<" ";
-            Q.pop();
+            while(!Q.empty()){
+                
+                cout<<Q.top().first<<" ";
+                int x=Q.top().second.first;
+                int y=Q.top().second.second;
+                Q.pop();
+                if(x<N && y+1<N){
+                    P temp;
+                    temp.first=arr[x][y+1];
+                    temp.second.first=x;
+                    temp.second.second=y+1;
+                    Q.push(temp);
+                }
+            }
+            cout<<endl;  
         }
-      cout<<endl;  
+      
     }
 
 
-}
+
