5ff26db62e569827fdda58a0394fb5004fedb665
sharnoor404 <sharnoor404@gmail.com>
2020-08-06 23:40:21 +0530

    GFG

--- Arrays/MergeTwoSortedWithoutExtraSpace_trial2.cpp
+++ Arrays/MergeTwoSortedWithoutExtraSpace_trial2.cpp
@@ -49,3 +49,49 @@ int main() {
 }
 
 //Approach 2:
+
+#include <iostream>
+#include<algorithm>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	for(int i=0;i<t;i++){
+	    int x,y;
+	    cin>>x>>y;
+	    int *arr1=new int[x];
+	    int *arr2=new int[y];
+	    for(int j=0;j<x;j++){
+	        cin>>arr1[j];
+	    }
+	    for(int j=0;j<y;j++){
+	        cin>>arr2[j];
+	    }
+	    
+	    int a=x-1;
+	    int b=0;
+	    while(a>=0 && b<y){
+	        if(arr1[a]>arr2[b]){
+	            //swap;
+	            int temp=arr1[a];
+	            arr1[a]=arr2[b];
+	            arr2[b]=temp;
+	        }
+	            a--;
+	            b++;
+	        
+	    }
+	    sort(arr1,arr1+x);
+	    sort(arr2,arr2+y);
+	    for(int j=0;j<x;j++){
+	        cout<<arr1[j]<<" ";
+	    }
+	    for(int j=0;j<y;j++){
+	        cout<<arr2[j]<<" ";
+	    }
+	    cout<<endl;
+	    
+	}
+	return 0;
+}
