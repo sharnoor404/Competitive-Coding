b274c5e2c2bd24d6bcca2307e8d84124ab4424ad
sharnoor404 <sharnoor404@gmail.com>
2020-08-07 19:41:49 +0530

    IB

--- Greedy Algorithm/Bulbs.cpp
+++ Greedy Algorithm/Bulbs.cpp
@@ -1,6 +1,8 @@
 /*
 Bulbs problem : IB
 */
+
+//Approach 1:
 int Solution::bulbs(vector<int> &A) {
     int look_for=0;
     int count=0;
@@ -16,4 +18,23 @@ int Solution::bulbs(vector<int> &A) {
     }
     return count;
 }
+
+//Approach 2:
+int Solution::bulbs(vector<int> &A) {
+    bool evenSwitches=true;
+    int count=0;
+  for(int i=0;i<A.size();i++){
+      if(A[i]==0 && evenSwitches){
+        A[i]=1;
+        evenSwitches=false;
+        count++;
+    }else if(A[i]==1 && !evenSwitches){
+        evenSwitches=true;
+        count++;
+    }
+  }
+    return count;
+}
+  
+
 
