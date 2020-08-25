edc3a8df13232e8debb3e1114592f47493063814
sharnoor404 <sharnoor404@gmail.com>
2020-08-25 20:01:51 +0530

    IB

---
+++ Math/ExcelColumnNumber.cpp
@@ -0,0 +1,13 @@
+/*
+Excel Column Number : IB
+*/
+
+int Solution::titleToNumber(string s) {
+    
+  int result = 0;
+       for (int i = 0; i < s.size(); i++) {
+               result = result * 26 + (s[i] - 'A' + 1);
+       }
+       return result;
+   
+}
