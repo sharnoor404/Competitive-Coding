003fb46b156db9bc1ff4fddd63d32a9a489ed492
sharnoor404 <sharnoor404@gmail.com>
2020-08-27 00:04:38 +0530

    GFG

--- DP 1/MinimumStepsTo1.cpp
+++ DP 1/MinimumStepsTo1.cpp
@@ -22,3 +22,32 @@ int countStepsToOne(int n)
     return std::min(x,std::min(y,z))+1;
 }
 
+//Approach 2: 
+#include<climits>
+int countStepsToOne(int n)
+{
+	int *arr=new int[n+1];
+    for(int i=0;i<=n;i++){
+        arr[i]=0;
+    }
+	
+    for(int i=2;i<=n;i++)
+    {
+        int x=INT_MAX,y=INT_MAX,z=INT_MAX;
+        if(i%3==0 && i/3>0){
+            x=arr[i/3];
+        }
+        if(i%2==0 && i/2>0){
+            y=arr[i/2];
+        }
+        if(i-1>0){
+             z=arr[i-1];
+        }
+           
+        arr[i]=std::min(x,std::min(y,z))+1;
+    }
+    int output=arr[n];
+    delete[] arr;
+    return output;
+}
+
