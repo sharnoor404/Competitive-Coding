c52d5628dd8170e620e752d4f590605277f7ec36
sharnoor404 <sharnoor404@gmail.com>
2020-08-30 00:55:32 +0530

    GFG

---
+++ Trees/LowestCommonAncestorInABT.cpp
@@ -0,0 +1,26 @@
+/*
+Lowest common ancestor in a binary tree : GFG
+*/
+
+Node* lca(Node* root ,int n1 ,int n2 )
+{
+    if(root==NULL){
+        return NULL;
+    }
+    if(root->data==n1 || root->data==n2){
+        return root;
+    }
+     Node* left=lca(root->left,n1,n2);
+     Node* right=lca(root->right,n1,n2);
+     if(left==NULL && right==NULL){
+         return NULL;
+     }
+     if(left!=NULL && right !=NULL){
+         return root;
+     }
+     if(left==NULL && right!=NULL){
+         return right;
+     }else{
+         return left;
+     }
+}
