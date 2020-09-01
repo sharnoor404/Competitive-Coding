0786c7f92d5f5823fe5a5341876491c009111674
sharnoor404 <sharnoor404@gmail.com>
2020-09-01 20:58:54 +0530

    CB

--- DP/LIS.cpp
+++ DP/LIS.cpp
@@ -1,32 +1,43 @@
 /*
-LIS : CN
+LIS : All approaches (CB)
 */
-
+#include <iostream>
 using namespace std;
 
- int lis(int arr[], int n) {
-		
-	int *LIS_till_i=new int[n];
-     LIS_till_i[0]=1;
-     for(int i=1;i<n;i++){
-         
-         int j=i;
-         int max=0;
-         while(j>=0){
-             if(arr[i]>arr[j] && (LIS_till_i[j])>max){
-                 max=(LIS_till_i[j]);
-             }
-             j--;
-         }
-         LIS_till_i[i]=max+1;
-     }
-     int max=0;
-     for(int i=0;i<n;i++){
-         //cout<<LIS_till_i[i]<<" ";
-         if(LIS_till_i[i]>max){
-             max=LIS_till_i[i];
-         }
-     }
-    // cout<<endl;
-     return max;
- }
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int *arr=new int[n];
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        int *dp=new int[n];
+        dp[0]=1;
+        int max_val=0;
+        for(int i=1;i<n;i++){
+            int j=i-1;
+            max_val=1;
+            for(;j>=0;j--){
+                if(arr[i]>arr[j] && (dp[j]+1>max_val)){
+                    max_val=dp[j]+1;
+                }
+            }
+            dp[i]=max_val;
+        }
+        max_val=0;
+        for(int i=0;i<n;i++){
+            if(dp[i]>max_val){
+                max_val=dp[i];
+            }
+        }
+        
+        delete [] arr;
+        delete [] dp;
+        
+        cout<<max_val<<endl;
+    }
+	return 0;
+}
